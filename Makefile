1a6edb63541bf24a2c236ff3dc20686f0709be12
Jean-Didier PAILLEUX <jean-didier.pailleux@ens.uvsq.fr>
2019-01-09 12:57:11 +0100

    Step 3

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC = gcc
 CFLAGS = -W -Wall
-LDFLAGS = -llapack
+LDFLAGS = -llapack -lm
 EXEC = PRR
 
 all: $(EXEC)
--- PRR.c
+++ PRR.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 #include "lapacke.h"
 
 double frand_a_b(double a, double b){
@@ -15,10 +16,43 @@ void init_vector(double* x, int size){
 		x[i] = frand_a_b(1.0, 1000.0);
 		printf("%0.3f  ", x[i]);
 	}
+	printf("\n");
+}
+
+/* Calcul de la norme d'un vecteur */
+double vect_norm(double* x, int size){
+	double result = 0;
+	for(int i = 0; i < size; i ++){
+		result = x[i]*x[i];
+	}
+
+	return sqrt(result);
+}
+
+/* Normalisation d'un vecteur */
+void normalize(double*x, double norm, int size){
+	for(int i = 0; i < size; i++){
+		x[i] = x[i]/norm;
+	}
 }
 
 /* Fonction Algorithme itérative PRR */
-void PRR(int m, int* x){
+void PRR(int m, double* x){
+
+	// Normalisation de x + calcul de y0
+	double* y = x;
+	double norm = vect_norm(y, m);
+	normalize(y, norm, m);
+	
+	// C0 = || y0 ||^2
+	double C1, C2;
+	norm = vect_norm(y, m);
+	C1 = norm*norm;
+	if ((int)C1 == 1) {
+		printf("OK\n");
+	}
+
+
 
 }
 
@@ -28,8 +62,8 @@ int main(int argc, char** argv){
 	int m = 10;
 	double* v = malloc(sizeof(double)*m);
 	srand(time(0));
-	
-	init_vector(v, m);
 
+	init_vector(v, m);
+	PRR(m, v);
 	return 0;	
 }
